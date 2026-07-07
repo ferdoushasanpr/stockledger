3ef36d1479c54e7b0750faa26abc27beb5263d46
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-07-07 17:41:53 +0600

    feat: Customer Model Added.

---
+++ lib/models/customer.dart
@@ -0,0 +1,15 @@
+class Customer {
+  const Customer({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.address,
+  });
+
+  final int id;
+  final String name;
+  final String email;
+  final String phone;
+  final String address;
+}
