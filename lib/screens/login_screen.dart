16619569a518dafa33e57ef88900ceb91358821d
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-07-08 22:24:55 +0600

    update: Validation Added to Login Screen.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,46 @@
 import "package:flutter/material.dart";
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _loginHandler() {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    final String username = _usernameController.text.trim();
+    final String password = _passwordController.text.trim();
+
+    if (username != "admin123" || password != "admin123") {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            "Invalid username or password. Please try again.",
+            textAlign: TextAlign.center,
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return;
+    }
+    print("Login successful.");
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,69 +49,99 @@ class LoginScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "StockLedger",
-                style: TextStyle(
-                  color: Color(0xFFFFBF00),
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                "Inventory and Sales Management Systems",
-                style: TextStyle(
-                  color: Color.fromARGB(255, 212, 197, 171),
-                  fontSize: 16,
-                ),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Username",
-                  filled: true,
-                  fillColor: Color(0xFF1E1E1E),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Password",
-                  filled: true,
-                  fillColor: Color(0xFF1E1E1E),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(8),
-                    borderSide: BorderSide.none,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFFFBF00),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                      2,
-                    ), // Slightly rounded corners
-                  ),
-                ),
-                child: Text(
-                  "LOGIN",
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "StockLedger",
                   style: TextStyle(
-                    color: Color(0xFF1E1E1E),
-                    fontSize: 16,
-                    fontWeight: FontWeight.normal,
+                    color: Color(0xFFFFBF00),
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-            ],
+                Text(
+                  "Inventory and Sales Management Systems",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 212, 197, 171),
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Username",
+                    filled: true,
+                    fillColor: Color(0xFF1E1E1E),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  style: TextStyle(color: Colors.white70),
+                  controller: _usernameController,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return "Username is required";
+                    }
+
+                    if (value.length < 4) {
+                      return "Username must be at least 4 characters";
+                    }
+
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    filled: true,
+                    fillColor: Color(0xFF1E1E1E),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                  style: TextStyle(color: Colors.white70),
+                  controller: _passwordController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Password is required";
+                    }
+
+                    if (value.length < 4) {
+                      return "Password must be at least 4 characters";
+                    }
+
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _loginHandler,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFFFFBF00),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                        2,
+                      ), // Slightly rounded corners
+                    ),
+                  ),
+                  child: Text(
+                    "LOGIN",
+                    style: TextStyle(
+                      color: Color(0xFF1E1E1E),
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
